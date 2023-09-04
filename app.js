466b6ac1d4e2e35f6e94c29cf224a9332dbc5e05
jnavarro30 <jnavarro30@live.com>
2023-09-04 14:46:29 -0700

    add jquery ui and datepicker

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let headerContent = document.querySelector("h1").textContent;
-document.querySelector("h1").textContent = headerContent.toUpperCase();
-setInterval(() => {
-  if (document.querySelector("body").style.backgroundColor == "blue") {
-    document.querySelector("body").style.backgroundColor = "green";
-  } else {
-    document.querySelector("body").style.backgroundColor = "blue";
-  }
-}, 2000);
+// let headerContent = document.querySelector("h1").textContent;
+// document.querySelector("h1").textContent = headerContent.toUpperCase();
+// setInterval(() => {
+//   if (document.querySelector("body").style.backgroundColor == "blue") {
+//     document.querySelector("body").style.backgroundColor = "green";
+//   } else {
+//     document.querySelector("body").style.backgroundColor = "blue";
+//   }
+// }, 2000);
+
+
+$("#datepicker").datepicker();
